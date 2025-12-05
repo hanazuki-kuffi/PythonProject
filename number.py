9981b9d5dd6da5763e2ccb3de591d927bc5312d3
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2025-12-05 19:55:46 +0500

    new 22 step number.py

--- number.py
+++ number.py
@@ -1,204 +1,219 @@
-# num_seven = 7
-# num_ten = 10
-# num_seventeen = 17
-# print(num_seventeen)
-from urllib.parse import non_hierarchical
-
-# a = 5
-# b = 4
-# print(a+b)
-# print(a-b)
-# print(a*b)
-# print(a/b)
-
-# g = '2004'  # естб какие нибудь разница между кавычкой или просто написав число
-# year = int(g)
-# print(year)
-
-# num1 = input()
-# num2 = input()
-# print(num1+num2)
-
-# num1 = int(input())
-# num2 = int(input())
-# print(num1+num2)
-
-# s = 0
-# k = 30
-# d = k - 5
-# k = 2 * d
-# s = k - 100
+num_seven = 7
+num_ten = 10
+num_seventeen = 17
+print(num_seventeen)
+# from urllib.parse import non_hierarchical
 #
-# print(s)
-
-# x = 3
-# y = 4
-# z = x + y
-# z = z + 1
-# x = y
-# y = 5
-# x = z + y + 7
+a = 5
+b = 4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
 #
-# print(x)
-
-# a = int(input())
-# b = a + 1
-# c = a + 2
+g = '2004'  # естб какие нибудь разница между кавычкой или просто написав число
+year = int(g)
+print(year)
 #
-# print(a)
-# print(b)
-# print(c)
-
-# a =  int(input())
-# b = a + 1
-# c = a + 2
+num1 = input()
+num2 = input()
+print(num1+num2)
 #
-# print(a)
-# print(b)
-# print(c)
-
-# a = int(input())
-# b = a + 1
-# c = a + 2
-# print(a)
-# print(b)
-# print(c)
-
-# a = int(input())
-# b = a + 1
-# c = a + 2
-# print(a)
-# print(b)
-# print(c)
-
-# a =  int(input())
-# b = a + 1
-# c = a + 2
-# print(a)
-# print(b)
-# print(c)
-
-
-# a = int(input())
-# b = int(input())
-# c = int(input())
-# x = a + b + c
-# print(x)
-
-# monitor = int(input())
-# system_unit = int(input())
-# keyboard = int(input())
-# mouse = int(input())
-# x = (monitor + system_unit + keyboard + mouse) * 3
-# print(x)
-
-# a = int(input())
-# b = int(input())
-# x = 3 * (a + b) * (a + b) * (a + b) + 275 * b * b - 127 * a - 41
-# print(x)
-
-# лучге всего будет если ты берешь эти чисел на скобках так будет в программе понятнее потоу что программа читат в одном направлний
-# то есть слева и на право
-# a = int(input())
-# b = int(input())
-# x = (3 * (a + b) * (a + b) * (a + b)) + (275 * b * b) - (127 * a) - 41
-# print(x)
-
-# twenty = int(input())
-# next = "Следующее за числом 20 число: 21"
-# previous = "Для числа 20 предыдущее число: 19"
-# print(twenty)
-# print(next)
+num1 = int(input())
+num2 = int(input())
+print(num1+num2)
 #
-# print(previous)
+s = 0
+k = 30
+d = k - 5
+k = 2 * d
+s = k - 100
 
-
-# number = int(input())
-# next_to = 'Следующее за числом  number, 'число:', number + 1
-# previous = 'Для числа', number, 'предыдущее число:', number - 1
+print(s)
 #
-# print(next_to)
-# print(previous)
+x = 3
+y = 4
+z = x + y
+z = z + 1
+x = y
+y = 5
+x = z + y + 7
 
+print(x)
+#
+a = int(input())
+b = a + 1
+c = a + 2
 
-# SHOULD BE A LOT OF PRACTICE
-# number = int(input())
-# next_to = number + 1
-# previous = number - 1
-# print('Следующее за числом', number, 'число:', next_to)
-# print('Для числа', number, 'предыдущее число:', previous)
+print(a)
+print(b)
+print(c)
+#
+a =  int(input())
+b = a + 1
+c = a + 2
 
+print(a)
+print(b)
+print(c)
+#
+a = int(input())
+b = a + 1
+c = a + 2
+print(a)
+print(b)
+print(c)
+#
+a = int(input())
+b = a + 1
+c = a + 2
+print(a)
+print(b)
+print(c)
+#
+a =  int(input())
+b = a + 1
+c = a + 2
+print(a)
+print(b)
+print(c)
+#
+#
+a = int(input())
+b = int(input())
+c = int(input())
+x = a + b + c
+print(x)
+#
+monitor = int(input())
+system_unit = int(input())
+keyboard = int(input())
+mouse = int(input())
+x = (monitor + system_unit + keyboard + mouse) * 3
+print(x)
+#
+a = int(input())
+b = int(input())
+x = 3 * (a + b) * (a + b) * (a + b) + 275 * b * b - 127 * a - 41
+print(x)
+#
+лучге всего будет если ты берешь эти чисел на скобках так будет в программе понятнее потоу что программа читат в одном направлний
+то есть слева и на право
+a = int(input())
+b = int(input())
+x = (3 * (a + b) * (a + b) * (a + b)) + (275 * b * b) - (127 * a) - 41
+print(x)
+#
+twenty = int(input())
+next = "Следующее за числом 20 число: 21"
+previous = "Для числа 20 предыдущее число: 19"
+print(twenty)
+print(next)
 
-# SHOULD BE A LOT OF PRACTICE
-# rib_length = int(input())
-# V = rib_length * rib_length * rib_length
-# S = 6 * (rib_length * rib_length)
-# print('Объем =', V)
-# print('Площадь полной поверхности =', S)
+print(previous)
+#
+#
+number = int(input())
+next_to = 'Следующее за числом  number, 'число:', number + 1
+previous = 'Для числа', number, 'предыдущее число:', number - 1
 
-
-# SHOULD BE A LOT OF PRACTICE
-# a = int(input())
-# b = int (input())
-# print(a, '+', b, '=', a + b)
-# print(a, '-', b, '=', a - b)
-# print(a, '*', b, '=', a * b)
-
-
-# SHOULD BE A LOT OF PRACTICE
-# a1 = int(input())
-# d = int(input())
-# n = int(input())
-# aN = a1 + (d * (n - 1))
-# print(aN)
-
-
-# SHOULD BE A LOT OF PRACTICE
-# x = int(input())
-# print(x, 2 * x,3 * x,4 * x,5 * x, sep='---')
-
-
-# SHOULD BE A LOT OF PRACTICE
-# b1 = int(input())
-# q = int(input())
-# n = int(input())
-# bn = b1 * q**(n - 1)
-# print(bn)
-
-
-
-# SHOULD BE A LOT OF PRACTICE
-# cm = int(input())
-# meter = cm // 100
-# print(meter)
-
-
-
-# n_stud = int(input())
-# k_orange = int(input())       n = 100%   /2 четные и нечетные числа
-#                               survivors = 50%
-# print(k_orange // n_stud)
-# print(k_orange % n_stud)
-
-# SHOULD BE A LOT OF PRACTICE
-# n = int(input())
-# print((n + 1) // 2)
-
-# SHOULD BE A LOT OF PRACTICE
-# inomin = int(input())
-# tonohours = (inomin//60)
-# remnominutes = (inomin % 60)
-# print(inomin,'min - the', tonohours, 'hour', remnominutes, 'minutes')
-
+print(next_to)
+print(previous)
+#
+#
+SHOULD BE A LOT OF PRACTICE
+number = int(input())
+next_to = number + 1
+previous = number - 1
+print('Следующее за числом', number, 'число:', next_to)
+print('Для числа', number, 'предыдущее число:', previous)
+#
+#
+SHOULD BE A LOT OF PRACTICE
+rib_length = int(input())
+V = rib_length * rib_length * rib_length
+S = 6 * (rib_length * rib_length)
+print('Объем =', V)
+print('Площадь полной поверхности =', S)
+#
+#
+SHOULD BE A LOT OF PRACTICE
+a = int(input())
+b = int (input())
+print(a, '+', b, '=', a + b)
+print(a, '-', b, '=', a - b)
+print(a, '*', b, '=', a * b)
+#
+#
+SHOULD BE A LOT OF PRACTICE
+a1 = int(input())
+d = int(input())
+n = int(input())
+aN = a1 + (d * (n - 1))
+print(aN)
+#
+#
+SHOULD BE A LOT OF PRACTICE
+x = int(input())
+print(x, 2 * x,3 * x,4 * x,5 * x, sep='---')
+#
+#
+SHOULD BE A LOT OF PRACTICE
+b1 = int(input())
+q = int(input())
+n = int(input())
+bn = b1 * q**(n - 1)
+print(bn)
+#
+#
+#
+SHOULD BE A LOT OF PRACTICE
+cm = int(input())
+meter = cm // 100
+print(meter)
+#
+#
+#
+n_stud = int(input())
+k_orange = int(input())       n = 100%   /2 четные и нечетные числа
+                              survivors = 50%
+print(k_orange // n_stud)
+print(k_orange % n_stud)
+#
+SHOULD BE A LOT OF PRACTICE
+n = int(input())
+print((n + 1) // 2)
+#
+SHOULD BE A LOT OF PRACTICE
+inomin = int(input())
+tonohours = (inomin//60)
+remnominutes = (inomin % 60)
+print(inomin,'min - the', tonohours, 'hour', remnominutes, 'minutes')
+#
 number_seat = int(input())
 seat = 36 - number_seat
 coupe = seat // 4
 print(9 - coupe)
-
-
-
-
-
+#
+number = int(input())
+last_digit = (number % 10)
+average_digit = (number % 100) // 10
+first_digit = number // 100
+adding_numbers = (last_digit + average_digit + first_digit)
+equation_numbers = (last_digit * average_digit * first_digit)
+print('Сумма цифр =', adding_numbers)
+print('Произведение цифр =', equation_numbers)
+#
+numbers = int(input())
+last_digit = (numbers % 10)
+average_digit = (numbers % 100) // 10
+first_digit = (numbers // 100)
+print(first_digit,average_digit,last_digit, sep='')
+print(first_digit,last_digit,average_digit, sep='')
+print(average_digit,first_digit,last_digit, sep='')
+print(average_digit,last_digit,first_digit, sep='')
+print(last_digit,first_digit,average_digit, sep='')
+print(last_digit,average_digit,first_digit, sep='')
 
 
 

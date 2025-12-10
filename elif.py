15439605ab9053ad0ebe8cf204b3126f54a51cf3
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2025-12-10 21:29:07 +0500

    conditional operator

--- elif.py
+++ elif.py
@@ -1,6 +1,24 @@
-num1 = int(input())
-num2 = int(input())
-if num1 < num2:
-    print(num1, 'less than', num2)
-if num1 > num2:
-    print(num1, 'more than', num2)
+# num1 = int(input())
+# num2 = int(input())
+# if num1 < num2:
+#     print(num1, 'less than', num2)
+# if num1 > num2:
+#     print(num1, 'more than', num2)
+#
+# if num1 == num2: #we use double quality
+#     print(num1, 'equals', num2)
+# if num1 != num2:
+#     print(num1, 'not equal', num2)
+
+
+# answer = input('What programming language are we learning?')
+# if answer == 'Python':
+#     print('That is right! You are genius c: c:')
+#     print('Python - an awesome language!')
+# else:
+#     print('Not quite like that!')
+
+
+age = int(input())
+if 3 <= age <= 6:
+    print('You child!')

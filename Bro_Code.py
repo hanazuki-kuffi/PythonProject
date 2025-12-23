373b0aa3804870f1bcab743dfb1fb11cfa7d63a2
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2025-12-23 20:38:20 +0500

    practice again

---
+++ Bro_Code.py
@@ -0,0 +1,76 @@
+# This is my first Python code!!
+# print("I like pizza!")
+# print("It is really good!")
+
+
+# Variable = A container for value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Kuffi"
+food = "Soup"
+email = "midorikuffi123@fake.com"
+
+# print(f"Hello {first_name}!")
+# print(f"You like {food}")
+# print(f"Your email: {email}")
+
+
+# Integers
+age = 21
+quantity = 4
+num_of_students = 16
+
+# print(f"You are {age} years old!")
+# print(f"You are buying {quantity} items!")
+# print(f"Your class has {num_of_students} students")
+
+# Float
+price = 10.99
+gpa = 3.1
+distance = 19.54
+
+# print(f"The price is: ${price}")
+# print(f"Your gpa is {gpa}")
+# print(f"You walk {distance}km")
+
+# Boolean
+
+is_student = False/True
+for_sale = False
+is_online = False
+
+# if is_online:
+#     print("You are online!")
+# else:
+#     print("You are offline")
+
+# if for_sale:
+#     print("That item is for sale")
+# else:
+#     print("That item is NOT available")
+
+
+# if is_student:
+#     print(f"You are student!")
+# else:
+#     print(f"You are NOT student!")
+# print(f" Are you a student?: {is_student}")
+
+
+# BRO COMMENT
+nickname = "Bro"
+year = 2025
+pi = 3.14
+is_admin = False
+
+# print(f"Your nickname: {nickname}")
+# print(f"It is {year} year")
+# print(f"Pi has a value: {pi}")
+# if is_admin:
+#     print("He is the admin of this channel")
+# else:
+#     print("He is NOT admin of this channel")
+
+
+

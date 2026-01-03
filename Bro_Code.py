ab92d9833285c07cfc0fa52d64ef13c142c9763c
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-01-03 23:05:36 +0500

    practice again

--- Bro_Code.py
+++ Bro_Code.py
@@ -7,9 +7,9 @@
 #            A variable behaves as if it was the value it contains
 
 # Strings
-first_name = "Kuffi"
-food = "Soup"
-email = "midorikuffi123@fake.com"
+# first_name = "Kuffi"
+# food = "Soup"
+# email = "midorikuffi123@fake.com"
 
 # print(f"Hello {first_name}!")
 # print(f"You like {food}")
@@ -17,18 +17,18 @@ email = "midorikuffi123@fake.com"
 
 
 # Integers
-age = 21
-quantity = 4
-num_of_students = 16
+# age = 21
+# quantity = 4
+# num_of_students = 16
 
 # print(f"You are {age} years old!")
 # print(f"You are buying {quantity} items!")
 # print(f"Your class has {num_of_students} students")
 
 # Float
-price = 10.99
-gpa = 3.1
-distance = 19.54
+# price = 10.99
+# gpa = 3.1
+# distance = 19.54
 
 # print(f"The price is: ${price}")
 # print(f"Your gpa is {gpa}")
@@ -36,9 +36,9 @@ distance = 19.54
 
 # Boolean
 
-is_student = False/True
-for_sale = False
-is_online = False
+# is_student = False/True
+# for_sale = False
+# is_online = False
 
 # if is_online:
 #     print("You are online!")
@@ -59,10 +59,10 @@ is_online = False
 
 
 # BRO COMMENT
-nickname = "Bro"
-year = 2025
-pi = 3.14
-is_admin = False
+# nickname = "Bro"
+# year = 2025
+# pi = 3.14
+# is_admin = False
 
 # print(f"Your nickname: {nickname}")
 # print(f"It is {year} year")
@@ -72,5 +72,51 @@ is_admin = False
 # else:
 #     print("He is NOT admin of this channel")
 
+#TYPECASTING
+
+#typecasting = the process of converting a variable from one date type to another
+#           srt(), int(), float(), boolean()
+
+# name = ""
+# age = 21
+# gpa = 3.1
+# is_student = False
+# print(type(is_student))
+
+# SOME EXAMPLES
+# 1) gpa = int(gpa)
+# print(gpa)           ((age += 1 =====  age = age + 1))---> does the same thing
+
+# 2)age = float(age)
+# print(age)
+
+# 3) age = str(age)
+# age += "1"
+# print(age)
+#print(type(age))
+
+# 4)  name = bool(name)     "Kuffi code", "K" --- если написать тут любой хреновых значений то на оутпуте всегда выходит как TRUE даже если ты не пишеш ничего просто оставишь пробел все равно выходит одно и тоже. а если ты там тут буквально ниченго не напишеш то в итоге выхолит как FALSE вот пример ""
+# print(name)
 
 
+# INPUT = A function that promts the user to enter data
+#      Returns the entered data as a string
+
+# name = input("What is your name?: ")
+# age = input("How old are you?: ")
+# age = int(age)
+# age += 1    # or: age = age + 1
+#
+# print(f"Hello, {name}!")
+# print("HAPPY BIRTHDAY!!!")
+# print(f"You are {age} years old")
+
+
+#EXERCISE 1 RECTANGLE AREA CALC
+
+# length = float(input("Enter the length: "))  # кароче тут такое дело без float на оутпуте не выходит ничего кроме ошибки там инпуте мы якобы пишим строка а не интеджер поэтому нам следует написать такие функций как это типо int(), float()
+
+# width = float(input("Enter the width: "))
+# area = length * width
+#
+# print(f"The area is: {area} cm²")

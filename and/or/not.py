f774a04c2325a958bcd8313ff988e4d64ed9727d
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-01-18 23:21:04 +0500

    practice again

---
+++ and/or/not.py
@@ -0,0 +1,7 @@
+# We have combined two conditions using the and operators. This means that in this branch, the code block is executed only if both conditions are met  simultaneously!
+# age = int(input("How old are you? "))
+# grade = int(input("What grade are you in? "))
+# if age >= 12 and grade >= 7:
+#     print("Access allowed!")
+# else:
+#     print("Access denied!")

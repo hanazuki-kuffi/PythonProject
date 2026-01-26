654140658ad9254d4e348f809622dc3b9451e49f
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-01-26 23:02:04 +0500

    practice again

--- Bro_Code.py
+++ Bro_Code.py
@@ -237,30 +237,30 @@ import math # Подключаем модуль math, который содер�
 # print(num1 + num2)
 
 
-# operator = input("Enter an operator (+ - / *): ")
-# num1 = float(input("Enter the 1st number: "))
-# num2 = float(input("Enter the 2nd number: "))
-#
-# print(num1 * num2)
+operator = input("Enter an operator (+ - / *): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
 
-# operator = input("Enter an operator (+ - / *): ")
-# num1 = float(input("Enter the 1st number: "))
-# num2 = float(input("Enter the 2nd number: "))
+print(num1 * num2)
 #
-# if operator == "+":
-#     result = num1 + num2
-#     print(result)
-# elif operator == "-":
-#     result = num1 - num2
-#     print(result)
-# elif operator == "*":
-#     result = num1 * num2
-#     print(result)
-# elif operator == "/":
-#     result = num1 / num2
-#     print(result)
+operator = input("Enter an operator (+ - / *): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
 
-# operator = input("Enter an operator (+ - / *): ")
+if operator == "+":
+    result = num1 + num2
+    print(result)
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+elif operator == "/":
+    result = num1 / num2
+    print(result)
+#
+operator = input("Enter an operator (+ - / *): ")
 # num1 = float(input("Enter the 1st number: "))
 # num2 = float(input("Enter the 2nd number: "))
 #

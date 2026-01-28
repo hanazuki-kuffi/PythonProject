db8a108ab31c21e726a1f095e382849b42b0e4a2
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-01-28 20:47:05 +0500

    practice again

--- and/dictionary.py
+++ and/dictionary.py
@@ -1,15 +1,15 @@
-# The following simple dictionary stores information about one specific alien
-#The alien_0 dictionary stores two attributes: color and points. The following two print commands read this information from the dictionary and display it on the screen:
-# alien_0 = {"colour": "Green", "points": 5}
-#
-# print(alien_0 ["colour"])
-# print(alien_0 ["points"])
+The following simple dictionary stores information about one specific alien
+The alien_0 dictionary stores two attributes: color and points. The following two print commands read this information from the dictionary and display it on the screen:
+alien_0 = {"colour": "Green", "points": 5}
 
-# alien_0 = {"colour": "green", "points": 5}
+print(alien_0 ["colour"])
+print(alien_0 ["points"])
 #
-# new_points = alien_0["points"]
-# print(f"You just earned {new_points} points!")
+alien_0 = {"colour": "green", "points": 5}
 
+new_points = alien_0["points"]
+print(f"You just earned {new_points} points!")
+#
 alien_0 = {"colour": "green", "points": 5}
 print(alien_0)
 

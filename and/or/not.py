f9d704375808b582f9744771a16a7444cfcc3c99
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-02-12 22:23:25 +0500

    practice again

--- and/or/not.py
+++ and/or/not.py
@@ -241,3 +241,19 @@ else:
     else:
         print("Third quarter")
 #
+
+grade = int(input('Введите вашу отметку по 100-балльной системе: '))
+
+if grade >= 90:
+    print(5)
+else:
+    if grade >= 80:
+        print(4)
+    else:
+        if grade >= 70:
+            print(3)
+        else:
+            if grade >= 60:
+                print(2)
+            else:
+                print(1)

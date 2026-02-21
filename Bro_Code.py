21b7e9977d3f95b6f37133a6feb47731d0675ee2
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-02-21 11:25:05 +0500

    practice again

--- Bro_Code.py
+++ Bro_Code.py
@@ -415,13 +415,24 @@ from turtledemo.sorting_animate import enable_keys
 # 2. username must not contain spaces
 # 3. username must not contain digits
 
-username = input("Enter a username: ")
+# username = input("Enter a username: ")
+#
+# if len(username) > 12:
+#     print("Your username can't be more than 12 characters!")
+# elif username.find(" "):
+#     print("Your username must not contain spaces!")
+# elif not username.isalpha():
+#     print("Your username must not contain numbers!")
+# else:
+#     print(f"Welcome {username}")
 
-if len(username) > 12:
-    print("Your username can't be more than 12 characters!")
-elif not username.find(""):                    # "" и " "
-    print("Your username must not contain spaces!")
-elif not username.isalpha():
-    print("Your username must not contain numbers!")
-else:
-    print(f"Welcome {username}")
+# username = input("Enter a username:")
+#
+# if len(username) > 12:
+#     print("Your username can't be more than 12 characters!")
+# elif not username.find(" ") == -1:
+#     print("Your username can't contain spaces!")
+# elif not username.isalpha():
+#     print("Your username can't contain numbers")
+# else:
+#     print(f"Welcome {username}!")
--- and/dictionary.py
+++ and/dictionary.py
@@ -150,3 +150,19 @@
 #
 # for name in favorite_languages.keys():
 #     print(name.title())
+
+# favorite_languages = {
+#     "jen": "python",
+#     "sarah": "c",
+#     "edward": "ruby",
+#     "phil": "python",
+# }
+#
+# friends = ["sarah", "phil"]
+# for name in favorite_languages.keys():
+#     print(name.title())
+#
+# if name in friends:
+#     language = favorite_languages[name].title()
+#     print(f"\t {name.title()}, I see you love {language}!")
+#ВАЩЕ НИХЕРА НЕ ПОН БРОУ ЮЛЯЯЯЯ#

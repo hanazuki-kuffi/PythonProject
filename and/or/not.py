d6d2a501ec2f1ae9f09745f081d20fad57a0ab0e
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-02-23 22:27:32 +0500

    practice again

--- and/or/not.py
+++ and/or/not.py
@@ -452,7 +452,7 @@ from termios import INPCK
 
 month_ordinal_number = int(input("Enter the month ordinal number: "))
 
-if month_ordinal_number == 1,3,5,7,8,10,12:
+if month_ordinal_number == in 1,3,5,7,8,10,12:
     print(31)
 if month_ordinal_number == 4,6,9,11:
     print(30)

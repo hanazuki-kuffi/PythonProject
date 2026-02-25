672be03bdac8b73841ee5ace5672e290715f3359
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-02-25 17:01:05 +0500

    practice again

--- and/dictionary.py
+++ and/dictionary.py
@@ -104,7 +104,7 @@
 # print(sister_07["Last_name"])
 # print(sister_07["age"])
 # print(sister_07["city"])
-
+#
 
 # favorite_number = {
 #     "Mother": 7,
@@ -151,18 +151,17 @@
 # for name in favorite_languages.keys():
 #     print(name.title())
 
-# favorite_languages = {
-#     "jen": "python",
-#     "sarah": "c",
-#     "edward": "ruby",
-#     "phil": "python",
-# }
-#
-# friends = ["sarah", "phil"]
-# for name in favorite_languages.keys():
-#     print(name.title())
-#
-# if name in friends:
-#     language = favorite_languages[name].title()
-#     print(f"\t {name.title()}, I see you love {language}!")
+favorite_languages = {
+    "jen": "python",
+    "sarah": "c",
+    "edward": "ruby",
+    "phil": "python",
+}
+
+friends = ["sarah", "phil"]
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
 #ВАЩЕ НИХЕРА НЕ ПОН БРОУ ЮЛЯЯЯЯ#

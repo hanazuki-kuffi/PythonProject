dc36f8e1542ebf6a3834093ef354467d24172951
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-03-02 16:13:41 +0500

    practice again

---
+++ and/list.py
@@ -0,0 +1,55 @@
+# bicycles = ["trek", "cannondale", "redline", "specialized"]
+# print(bicycles)
+
+# bicycles = ["trek", "cannondale", "redline", "specialized"] # Список — упорядочённый набор. каждый элемент содержат свои индексы
+# print(bicycles[0:4])   #Когда мы запрашиваем один элемент из списка, Python возвращает только этот элемент без квадратных скобок или кавычек:
+
+# bicycles = ["trek", "cannondale", "redline", "specialized"] #Индексы начинаются с 0, а не с 1
+# print(bicycles[0].title())
+
+# bicycles = ["trek", "cannondale", "redline", "specialized"]
+# print(bicycles[0].title())
+# print(bicycles[3].title())
+
+# bicycles = ["trek", "cannondale", "redline", "specialized"] #Фрагмент вернет значение 'specialized'. Этот синтаксис весьма полезен, потому что при работе со списками часто требуется обратиться к последним элементам, не зная точного количества элементов в списке.
+# print(bicycles[-2])
+
+# bicycles = ["trek", "cannondale", "redline", "specialized"]
+# print(f"My first bicycle was a {bicycles[0].title()}")
+
+# my_fruits = ["apple", "banana", "lime"]
+# print(my_fruits)
+#
+# nummbers = [141, 245, 762, 123]
+# print(nummbers)
+#
+# user_inputs = [True, 151, "🌸", 10.5]
+# print(user_inputs)
+
+# my_fruits = ["apple", "banana", "lime"]
+#
+# other_fruits = ["banana", "lime", "strawberry"]
+#
+# print(my_fruits == other_fruits) #False сразу в оутпуте выходит потому что ПОРЯДОК ЭЛЕМЕНТОВ В СПИСКЕ ИМЕЕТ ЗНАЧЕНИЕ
+
+# empty_list = []
+# print(len(empty_list)) #  в оутпуте выходит 0
+#
+# my_fruits = ["apple", "banana", "lime"]
+# print(len(my_fruits)) # в оутпуте выходит 3
+#
+# posts_ids = [151, 141, 161, 171, 181]
+# print(len(posts_ids))  # в  оутпуте выходиит 5
+#
+# user_inputs = [True, "🌸", 141, 10.5, "Hello World!"]
+# print(len(user_inputs))  # тоже самое в оутпуте выходит 5
+# posts_ids = [141, 151, 161, 171]
+# print(posts_ids[0])
+# print(posts_ids[1])
+#
+# print(posts_ids[-1])
+# print(posts_ids[-2])
+#
+
+
+posts_ids =

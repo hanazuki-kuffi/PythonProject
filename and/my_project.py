d34cee818f2e7ca5ee63930e41f6df3369dd4937
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-03-06 18:01:54 +0500

    practice again

---
+++ and/my_project.py
@@ -0,0 +1,14 @@
+analysis = ["STI_test", "gelmoscrin", "hpv23", "streptococcus agalactae", "d-flor", "candida.albicans"]
+
+print("Available lab tests")
+
+# for number, test in enumerate(analysis, 1):
+#     print(f"{number}. {test.title()}")
+
+# num = 1
+# for analys in analysis:
+#     print(f"{num}. {analys.title()}")
+#     num = num + 1
+
+
+

549476bdafc2a61fcd9d2b0c84cbf67a006bbbd1
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-03-12 23:54:02 +0500

    practice again

--- and/list.py
+++ and/list.py
@@ -240,13 +240,13 @@
 # all_ratings = my_ratings + other_ratings
 # print(all_ratings) #При использований оператора + вызывается метод списков ___add___
 
-ratings = [2.5, 5.0, 4.3, 3.7, 4.5]
-
-first_two_ratings = ratings[:2]
-print(first_two_ratings) #[2.5, 5.0]
-
-middle_ratings = ratings[1:-1]
-print(middle_ratings) #[5.0, 4.3, 3.7]
-
-last_two_ratings = ratings[-2:]
-print(last_two_ratings) #[3.7, 4.5]
+# ratings = [2.5, 5.0, 4.3, 3.7, 4.5]
+#
+# first_two_ratings = ratings[:2]
+# print(first_two_ratings) #[2.5, 5.0]
+#
+# middle_ratings = ratings[1:-1]
+# print(middle_ratings) #[5.0, 4.3, 3.7]
+#
+# last_two_ratings = ratings[-2:]
+# print(last_two_ratings) #[3.7, 4.5]

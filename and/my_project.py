f74c2dc2dff63424dd98e9d94965bdd788174449
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-03-25 01:16:57 +0500

    practice again

--- and/my_project.py
+++ and/my_project.py
@@ -1,20 +1,20 @@
-# analysis = ["STI_test", "gelmoscrin", "hpv23", "streptococcus agalactae", "d-flor", "candida.albicans"]
-#
-# print("Available lab tests")
+analysis = ["STI_test", "gelmoscrin", "hpv23", "streptococcus agalactae", "d-flor", "candida.albicans"]
 
-# number = 1
-# for test in analysis:
-#
-#     print(f"{number}. {test}")
-#     number = number + 1
+print("Available lab tests")
+
+number = 1
+for test in analysis:
+
+    print(f"{number}. {test}")
+    number = number + 1
 
 
-# analysis = ["STI_test", "gelmoscrin", "hpv23", "streptococcus agalactae", "d-flor", "candida.albicans"]
-#
-# print("Available lab tests")
-#
-# for test, number in enumerate(analysis, 1):
-#     print(f"{test}. {number}")
+analysis = ["STI_test", "gelmoscrin", "hpv23", "streptococcus agalactae", "d-flor", "candida.albicans"]
+
+print("Available lab tests")
+
+for test, number in enumerate(analysis, 1):
+    print(f"{test}. {number}")
 
 
 PROJECT №1

84ec23dbf811d4931bacd4aa6dae2955b1a0214a
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-03-25 01:17:35 +0500

    practice again

--- and/my_project.py
+++ and/my_project.py
@@ -1,32 +1,32 @@
-analysis = ["STI_test", "gelmoscrin", "hpv23", "streptococcus agalactae", "d-flor", "candida.albicans"]
-
-print("Available lab tests")
-
-number = 1
-for test in analysis:
-
-    print(f"{number}. {test}")
-    number = number + 1
-
-
-analysis = ["STI_test", "gelmoscrin", "hpv23", "streptococcus agalactae", "d-flor", "candida.albicans"]
-
-print("Available lab tests")
-
-for test, number in enumerate(analysis, 1):
-    print(f"{test}. {number}")
-
-
-PROJECT №1
-
-cycle_threshold1 = input("enter a number: ")
-
-if cycle_threshold1 == " " or cycle_threshold1 == "  ":
-    print("Negative")
-elif 18 <= int(cycle_threshold1) <= 35:
-    print("Positive")
-elif 18 > int(cycle_threshold1) or 35 < int(cycle_threshold1):
-    print("Invalid")
+# analysis = ["STI_test", "gelmoscrin", "hpv23", "streptococcus agalactae", "d-flor", "candida.albicans"]
+#
+# print("Available lab tests")
+#
+# number = 1
+# for test in analysis:
+#
+#     print(f"{number}. {test}")
+#     number = number + 1
+#
+#
+# analysis = ["STI_test", "gelmoscrin", "hpv23", "streptococcus agalactae", "d-flor", "candida.albicans"]
+#
+# print("Available lab tests")
+#
+# for test, number in enumerate(analysis, 1):
+#     print(f"{test}. {number}")
+#
+#
+# PROJECT №1
+#
+# cycle_threshold1 = input("enter a number: ")
+#
+# if cycle_threshold1 == " " or cycle_threshold1 == "  ":
+#     print("Negative")
+# elif 18 <= int(cycle_threshold1) <= 35:
+#     print("Positive")
+# elif 18 > int(cycle_threshold1) or 35 < int(cycle_threshold1):
+#     print("Invalid")
 
 
 barcode_patients= ["202504", "302643", "505678", "343235", "414754", "354890"]

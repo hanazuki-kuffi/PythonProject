01683b287370801f0f3e695161bbb43141582778
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-03-27 23:18:51 +0500

    practice again

--- and/my_project.py
+++ and/my_project.py
@@ -120,14 +120,37 @@ for patient in ct_results:
     index = 0
     for channel in ct_channels:
 
-        cycle_treshold = patient[index]
+        cycle_threshold = patient[index]
 
-        if cycle_treshold == None:
+        if cycle_threshold == None:
             print(f"{channel}: Negative")
-        elif 18 <= cycle_treshold <= 35:
+        elif 18 <= cycle_threshold <= 35:
             print(f"{channel}: Positive")
-        elif 18 > cycle_treshold or cycle_treshold > 35:
+        elif 18 > cycle_threshold or cycle_threshold > 35:
             print(f"{channel}: Invalid")
 
         index += 1
     patient_index += 1
+
+
+barcode_patients.append(707890)
+print(barcode_patients)
+
+ct_results.append([None, 31.5, None, 28.0, None])
+print(ct_results)
+
+print("\n New patient 707890 added to the system")
+
+print(f"\n Patient {barcode_patients[-1]} has cancelled their test")
+
+barcode_patients.pop()
+print(barcode_patients)
+
+ct_results.pop(-1)
+print(ct_results)
+
+
+print("Results updated for patient 202552")
+
+ct_results[2] = [31.0, 29.5, 28.0, 27.5, None]
+print(ct_results)

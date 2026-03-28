4a2bf36d904e5c3e1179f911335ffcbee2c1c2d3
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-03-28 23:26:02 +0500

    practice again

--- and/my_project.py
+++ and/my_project.py
@@ -100,57 +100,57 @@ barcode_patients = [202504, 305463, 202552, 505508, 604125, 505123, 202654, 4145
 
 ct_channels = ["FAM", "HEX", "ROX", "CY5", "CY5.5"]
 
-ct_results = [
-    [28.11, 29.11, 30.67, 29.00, None],
-    [None, None, None, 28.11, None],
-    [29.91, None, 28.66, 41.00, None],
-    [None, None, None, 29.95, None],
-    [None, 34.95, None, 29.95, None],
-    [36.00, None, None, 25.89, None],
-    [None, None, None, 32.95, None],
-    [None, None, None, 29.91, None]
-
-]
-
-patient_index = 0
-
-for patient in ct_results:
-    print(f"\n Patient: {barcode_patients[patient_index]}")
-
-    index = 0
-    for channel in ct_channels:
-
-        cycle_threshold = patient[index]
-
-        if cycle_threshold == None:
-            print(f"{channel}: Negative")
-        elif 18 <= cycle_threshold <= 35:
-            print(f"{channel}: Positive")
-        elif 18 > cycle_threshold or cycle_threshold > 35:
-            print(f"{channel}: Invalid")
-
-        index += 1
-    patient_index += 1
-
-
-barcode_patients.append(707890)
-print(barcode_patients)
-
-ct_results.append([None, 31.5, None, 28.0, None])
-print(ct_results)
-
-print("\n New patient 707890 added to the system")
-
-print(f"\n Patient {barcode_patients[-1]} has cancelled their test")
-
-barcode_patients.pop()
-print(barcode_patients)
-
-ct_results.pop(-1)
-print(ct_results)
-
-
-print("Results updated for patient 202552")
-
-ct_results[2] = [31.0, 29.5, 28.0, 27.5, None]
-print(ct_results)
+# ct_results = [
+#     [28.11, 29.11, 30.67, 29.00, None],
+#     [None, None, None, 28.11, None],
+#     [29.91, None, 28.66, 41.00, None],
+#     [None, None, None, 29.95, None],
+#     [None, 34.95, None, 29.95, None],
+#     [36.00, None, None, 25.89, None],
+#     [None, None, None, 32.95, None],
+#     [None, None, None, 29.91, None]
+#
+# ]
+#
+# patient_index = 0
+#
+# for patient in ct_results:
+#     print(f"\n Patient: {barcode_patients[patient_index]}")
+#
+#     index = 0
+#     for channel in ct_channels:
+#
+#         cycle_threshold = patient[index]
+#
+#         if cycle_threshold == None:
+#             print(f"{channel}: Negative")
+#         elif 18 <= cycle_threshold <= 35:
+#             print(f"{channel}: Positive")
+#         elif 18 > cycle_threshold or cycle_threshold > 35:
+#             print(f"{channel}: Invalid")
+#
+#         index += 1
+#     patient_index += 1
+#
+#
+# barcode_patients.append(707890)
+# print(barcode_patients)
+#
+# ct_results.append([None, 31.5, None, 28.0, None])
+# print(ct_results)
+#
+# print("\n New patient 707890 added to the system")
+#
+# print(f"\n Patient {barcode_patients[-1]} has cancelled their test")
+#
+# barcode_patients.pop()
+# print(barcode_patients)
+#
+# ct_results.pop(-1)
+# print(ct_results)
+#
+#
+# print("Results updated for patient 202552")
+#
+# ct_results[2] = [31.0, 29.5, 28.0, 27.5, None]
+# print(ct_results)

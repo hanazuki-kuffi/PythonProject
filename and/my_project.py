7b052c1c40e141a6c3fa25af4c2a6c3d9661a581
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-04-03 17:30:24 +0500

    practice again

--- and/my_project.py
+++ and/my_project.py
@@ -29,14 +29,14 @@
 #     print("Invalid")
 
 
-# barcode_patients= ["202504", "302643", "505678", "343235", "414754", "354890"]
-#
-# ct_channels = ["FAM", "HEX", "ROX", "CY5", "CY5.5"]
-#
-# ct_results1 = [None, 22.40, None, 28.5, None]
-#
-#
-# # FOR 1 PATIENT
+barcode_patients = ["202504", "302643", "505678", "343235", "414754", "354890"]
+
+ct_channels = ["FAM", "HEX", "ROX", "CY5", "CY5.5"]
+
+ct_results1 = [None, 22.40, None, 28.5, None]
+
+
+# FOR 1 PATIENT
 # print(f"Patient: {barcode_patients[0]}")
 # index = 0
 # for channel in ct_channels:
@@ -52,12 +52,12 @@
 #     index = index + 1
 
 
-#FOR SOME PATIENT
+# FOR SOME PATIENTS
 
-# barcode_patients = ["202504", "302643", "505678", "343235", "414754", "354890"]
-# 
-# ct_channels = ["FAM", "HEX", "ROX", "CY5", "CY5.5"]
-# 
+barcode_patients = ["202504", "302643", "505678", "343235", "414754", "354890"]
+
+ct_channels = ["FAM", "HEX", "ROX", "CY5", "CY5.5"]
+
 # ct_results2 = [
 #     [29.51, 28.5, 28.11, 33.0, 28.79],
 #     [None, 23.95, None, 28.66, None],
@@ -66,23 +66,23 @@
 #     [None, None, None, 27.00, 33.9],
 #     [None, 24.00, None, 27.00, None]
 # ]
-# 
+#
 # patient_index = 0
 # for patient in ct_results2:
 #     print(f"\n Patient: {barcode_patients[patient_index]}")
-# 
+#
 #     index = 0
 #     for channel in ct_channels:
-# 
+#
 #         cycle_threshold = patient[index]
-# 
+#
 #         if cycle_threshold == None:
 #             print(f"{channel}: Negative")
 #         elif 18 <= cycle_threshold <= 35:
 #             print(f"{channel}: Positive")
 #         elif cycle_threshold < 18 or cycle_threshold > 35:
 #             print(f"{channel}: Invalid")
-# 
+#
 #         index += 1
 #     patient_index += 1
 
@@ -96,71 +96,71 @@
 
 
 
-barcode_patients = [202504, 305463, 202552, 505508, 604125, 505123, 202654, 414506]
-
-ct_channels = ["FAM", "HEX", "ROX", "CY5", "CY5.5"]
-
-ct_results = [
-    [28.11, 29.11, 30.67, 29.00, None],
-    [None, None, None, 28.11, None],
-    [29.91, None, 28.66, 41.00, None],
-    [None, None, None, 29.95, None],
-    [None, 34.95, None, 29.95, None],
-    [36.00, None, None, 25.89, None],
-    [None, None, None, 32.95, None],
-    [None, None, None, 29.91, None]
-
-]
-
-
-patient_index = 0
-
-positive_count = 0
-negative_count = 0
-invalid_count = 0
-
-for patient in ct_results:
-    print(f"\n Patient: {barcode_patients[patient_index]}")
-
-    index = 0
-    for channel in ct_channels:
-
-        cycle_threshold = patient[index]
-
-        if cycle_threshold == None:
-            print(f"{channel}: Negative")
-            negative_count += 1
-        elif 18 <= cycle_threshold <= 35:
-            print(f"{channel}: Positive")
-            positive_count += 1
-        elif 18 > cycle_threshold or cycle_threshold > 35:
-            print(f"{channel}: Invalid")
-            invalid_count += 1
-
-        index += 1
-    patient_index += 1
+# barcode_patients = [202504, 305463, 202552, 505508, 604125, 505123, 202654, 414506]
 #
-
-print("\n === SUMMARY ===")
-print(f"Total patients: {len(barcode_patients)}")
-print(f"Total positive: {positive_count}!")
-print(f"Total Negative: {negative_count}!")
-print(f"Total Invalid: {invalid_count}!")
+# ct_channels = ["FAM", "HEX", "ROX", "CY5", "CY5.5"]
 #
-barcode_patients.append(707890)
-print(barcode_patients)
-
-ct_results.append([None, 31.5, None, 28.0, None])
-print(ct_results)
-
-print("\n New patient 707890 added to the system")
-
-print(f"\n Patient {barcode_patients[-1]} has cancelled their test")
-
-barcode_patients.pop()
-print(barcode_patients)
-
-ct_results.pop(-1)
+# ct_results = [
+#     [28.11, 29.11, 30.67, 29.00, None],
+#     [None, None, None, 28.11, None],
+#     [29.91, None, 28.66, 41.00, None],
+#     [None, None, None, 29.95, None],
+#     [None, 34.95, None, 29.95, None],
+#     [36.00, None, None, 25.89, None],
+#     [None, None, None, 32.95, None],
+#     [None, None, None, 29.91, None]
+#
+# ]
+#
+#
+# patient_index = 0
+#
+# positive_count = 0
+# negative_count = 0
+# invalid_count = 0
+#
+# for patient in ct_results:
+#     print(f"\n Patient: {barcode_patients[patient_index]}")
+#
+#     index = 0
+#     for channel in ct_channels:
+#
+#         cycle_threshold = patient[index]
+#
+#         if cycle_threshold == None:
+#             print(f"{channel}: Negative")
+#             negative_count += 1
+#         elif 18 <= cycle_threshold <= 35:
+#             print(f"{channel}: Positive")
+#             positive_count += 1
+#         elif 18 > cycle_threshold or cycle_threshold > 35:
+#             print(f"{channel}: Invalid")
+#             invalid_count += 1
+#
+#         index += 1
+#     patient_index += 1
+# #
+#
+# print("\n === SUMMARY ===")
+# print(f"Total patients: {len(barcode_patients)}")
+# print(f"Total positive: {positive_count}!")
+# print(f"Total Negative: {negative_count}!")
+# print(f"Total Invalid: {invalid_count}!")
+# #
+# barcode_patients.append(707890)
+# print(barcode_patients)
+#
+# ct_results.append([None, 31.5, None, 28.0, None])
+# print(ct_results)
+#
+# print("\n New patient 707890 added to the system")
+#
+# print(f"\n Patient {barcode_patients[-1]} has cancelled their test")
+#
+# barcode_patients.pop()
+# print(barcode_patients)
+#
+# ct_results.pop(-1)
 # print(ct_results)
 #
 #
---
+++ while.py
@@ -0,0 +1,17 @@
+# name = input("Please enter your name: ")
+# print(f"Hello {name}!\n")
+
+# prompt = "If you tell us who you are, we can personalise the messages you see!"
+# prompt += "\nWhat is your first name?: "
+#
+# name = input(prompt)
+# print(f"\nHello {name}!")
+
+# age = input("How old are you?: ")
+# print(age)
+
+age = input("How old are you?: ")
+print(age)
+
+age = int(input())
+age >= 18

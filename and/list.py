3d8dfd99195f4a97bc56cf44b0c1bb76c6574cf8
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-04-06 08:21:10 +0500

    practice again

--- and/list.py
+++ and/list.py
@@ -589,5 +589,5 @@
 # for magician in magicians:
 #     print(f"{magician.title()}, that was a great trick!")
 #     print(f"I can not wait to see your next trick {magician.title()} \n")
-#
-# print("Thank you everyone. That was a great magic show!")
+
+print("Thank you everyone. That was a great magic show!")

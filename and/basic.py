a1e6ab023359d6929cad89d9200d2c3496667e18
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-04-06 12:58:12 +0500

    practice again

--- and/basic.py
+++ and/basic.py
@@ -8,10 +8,30 @@
 #
 # print_name("Kuffi")
 
-long_string = "This is a very long srting"
+# long_string = "This is a very long srting"
+#
+# print(long_string) #This is a very long srting
+#
+# print(type(long_string)) #<class 'str'>
+#
+# print(id(long_string))
 
-print(long_string) #This is a very long srting
+# info_msg = """You are learning the easiest programming language - Python!"""
+# print(info_msg)
+#
+# long_str = """This  #3 КАВЫЧКИ ПОЛЕЗЕН ДЛЯ ТОГО ЧТО БЫ НАПИСАТЬ ЧТО ТО ДЛЯ МНОГОКРАТНЫХ СТРОК
+#     is a very
+#        long string"""
+#
+# print(long_str)
+#
+# print(type(long_str))
+# print(id(long_str))
 
-print(type(long_string)) #<class 'str'>
-
-print(id(long_string))
+# my_name = "Gulaiym"
+#
+# print(len(my_name))
+#
+# print(my_name[2])
+#
+# print(my_name[3:8])
--- while.py
+++ while.py
@@ -1,5 +1,6 @@
 # name = input("Please enter your name: ")
 # print(f"Hello {name}!\n")
+# from http.cookiejar import user_domain_match
 
 # prompt = "If you tell us who you are, we can personalise the messages you see!"
 # prompt += "\nWhat is your first name?: "
@@ -23,11 +24,53 @@
 #     print("\nYou will be able to the ride when you are a little older.")
 
 
-number = input("Enter a number, and I will tell you if it is even or odd: ")
-number = int(number)
+# number = input("Enter a number, and I will tell you if it is even or odd: ")
+# number = int(number)
+#
+# if number % 2 == 0:
+#     print(f"The number {number} is even.\n")
+# else:
+#     print(f"The number {number} is odd.\n")
 
-if number % 2 == 0:
-    print(f"The number {number} is even.\n")
-else:
-    print(f"The number {number} is odd.\n")
+# car = input("What kind of car would he like to rent? ")
+# print(f"Let me see if I can find you a {car}.")
 
+
+# seats = input("How many seats does he/she want to reserve a table for in the restaurant? ")
+# seats = int(seats)
+#
+# if seats > 8:
+#     print("You will have to wait 😅!")
+# else:
+#     print("The table is ready ☺️!")
+
+# user_value = int(input("Enter a number: "))
+#
+# if user_value % 10 == 0:
+#     print("multiple value")
+# else:
+#     print("not a multiple of 10")
+
+
+# current_number = 1
+# while current_number <= 5:
+#     print(current_number)
+#     current_number += 1
+
+#
+# prompt = "\nTell me something and I will repeat it back to you. "
+# prompt += "\nEnter 'quit' to end the program: "
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#     print(message)
+
+
+prompt = "\nTell me something and I will repeat it back to you."
+prompt += "\nEnter 'quit' to end the program!: "
+message = ""
+
+while message != 'quit':
+    print(message)
+
+    if message !=

ca921a3e149969ff1491bed35691aac47ac46d3d
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-04-08 23:22:36 +0500

    practice again

--- while.py
+++ while.py
@@ -111,12 +111,34 @@ import random
 #     break #он говорит "выйди из цикла прямо сейчас".
 
 
-prompt = "\nTell me something and I will repeat it back to you."
-prompt += "\nEnter 'quit' to end the program!: "
+# prompt = "\nTell me something and I will repeat it back to you."
+# prompt += "\nEnter 'quit' to end the program!: "
+#
+# active = True
+# while active:
+#     message = input(prompt)
+#
+#     if message == 'quit':
+#         active = False
+#     else:
+#         print(message)
 
-active = True
-while active:
-    message = input(prompt)
 
-    if message == 'quit':
-        active = False
+# prompt = "\nPlease, enter the name of a city you have visited"
+# prompt += "\n Enter 'quit' when you are finished: "
+#
+# while True:
+#     city = input(prompt)
+#     if city == 'quit':
+#         break #Команда break может использоваться в любых циклах Python . Напри- мер, ее можно включить в цикл for для перебора элементов словаря .
+#     else:
+#         print(f"I would love to go to {city.title()}!")
+
+current_number = 0
+
+while current_number < 10:
+    current_number = current_number + 1
+
+    if current_number % 2 == 0:
+        continue
+    print(current_number)

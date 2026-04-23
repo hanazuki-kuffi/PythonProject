201a0e5ca86160052c52c701eafe4232273c0a2a
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-04-23 11:58:38 +0500

    practice again

--- while.py
+++ while.py
@@ -348,18 +348,18 @@ import random
 #     print(g)
 #     g = g + 1
 
-all_vacation = [] # создали пустой список, и скоро будем складывать элементы
-
-active = True # включен активация
-while active: # пока active = True цикл крутится бесконечно
-    current_survey = input("Where would you like to spend your vacation?: ") #каждый раз спрашивает у пользователя спрашивает ответы
-
-    if current_survey == "quit": #внесем логику если пользователь написал 'quit' и active = False остановливается цикл
-        active = False
-    else:
-        all_vacation.append(current_survey) # если пользователь не ввел 'quit' то цикл продолжается также ответы будут добавляться в список
-
-print(all_vacation) # после того цикл завершился с 'quit' мы выводим список все накопленнные ответы
+# all_vacation = [] # создали пустой список, и скоро будем складывать элементы
+#
+# active = True # включен активация
+# while active: # пока active = True цикл крутится бесконечно
+#     current_survey = input("Where would you like to spend your vacation?: ") #каждый раз спрашивает у пользователя спрашивает ответы
+#
+#     if current_survey == "quit": #внесем логику если пользователь написал 'quit' и active = False остановливается цикл
+#         active = False
+#     else:
+#         all_vacation.append(current_survey) # если пользователь не ввел 'quit' то цикл продолжается также ответы будут добавляться в список
+#
+# print(all_vacation) # после того цикл завершился с 'quit' мы выводим список все накопленнные ответы
 
 
 

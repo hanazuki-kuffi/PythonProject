f15d9bdf3386d62c16c43cb3fd97ac92a49834dd
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-05-08 15:57:53 +0500

    practice again

--- and/runtime.py
+++ and/runtime.py
@@ -1,4 +1,38 @@
 print("Hello new World")
 
 # print()- это функция
-#
+##1-задача
+##digit_1 = 3
+##digit_2 = 7
+##
+##solution_1 = (digit_1 ** 7)
+##print(solution_1)
+##
+##solution_2 = (digit_2 / digit_1)
+##print(solution_2)
+
+##2-задача
+
+##user_1 = 51
+##user_2 = 49
+##user_3 = 50
+##user_4 = 48
+##user_5 = 52
+##
+##average_user_age = ((user_1 + user_2 + user_3 + user_4 + user_5) / 5)
+##print(average_user_age)
+
+##3-задача
+
+mill_km = 12
+dollar_tg = 15
+tenge_dogicoin = 150000
+
+killometer = (mill_km * 1.60934)
+print(killometer)
+
+convertation_dollar_by_tenge = (dollar_tg * 464.62)
+print(convertation_dollar_by_tenge)
+
+convertation_tenge_by_dogicoin = (tenge_dogicoin * 0.020)
+print(convertation_tenge_by_dogicoin)

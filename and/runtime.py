00601d31c4d8d609be75d270831f75bb7842d9fc
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-05-09 13:24:58 +0500

    practice again

--- and/runtime.py
+++ and/runtime.py
@@ -2,41 +2,41 @@
 
 # print()- это функция
 ##1-задача
-##digit_1 = 3
-##digit_2 = 7
-##
-##solution_1 = (digit_1 ** 7)
-##print(solution_1)
-##
-##solution_2 = (digit_2 / digit_1)
-##print(solution_2)
-
-##2-задача
-
-##user_1 = 51
-##user_2 = 49
-##user_3 = 50
-##user_4 = 48
-##user_5 = 52
-##
-##average_user_age = ((user_1 + user_2 + user_3 + user_4 + user_5) / 5)
-##print(average_user_age)
-
-##3-задача
-
+# digit_1 = 3
+# digit_2 = 7
+#
+# solution_1 = (digit_1 ** 7)
+# print(solution_1)
+#
+# solution_2 = (digit_2 / digit_1)
+# print(solution_2)
+#
+# ##2-задача
+#
+# user_1 = 51
+# user_2 = 49
+# user_3 = 50
+# user_4 = 48
+# user_5 = 52
+#
+# average_user_age = ((user_1 + user_2 + user_3 + user_4 + user_5) / 5)
+# print(average_user_age)
+#
+# ##3-задача
+#
 # mill_km = 12
 # dollar_tg = 15
 # tenge_dogicoin = 150000
 #
 # killometer = (mill_km * 1.60934)
 # print(killometer)
-#
+# #
 # convertation_dollar_by_tenge = (dollar_tg * 464.62)
 # print(convertation_dollar_by_tenge)
 #
 # convertation_tenge_by_dogicoin = (tenge_dogicoin * 0.020)
 # print(convertation_tenge_by_dogicoin)
-
+#
 # a = 5
 # сube_area = 6 * a ** 2
 # print(сube_area)
@@ -45,38 +45,64 @@
 # print("Zharylkassynova Gulaiym Dumanovna, I want to became a good developer")
 
 
+# ##1-задача
+# my_name = input("Enter your name: ")
+# age = int()
+# my_weight = float(my_weight)
+# weather = str("20")
+#
+# ##2-задача
+# my_review = str("I am exited to continue learning programming")
+#
+# ##3-задача
+# my_money_now = float(input())
+# my_money_future = float(input())
+#
+# ##4-задача
+# my_age = int(input("Enter your age: ")
+# my_birthday = int(input("Enter your birthday: ")
+#
+#
+# ##5-задача
+# activ_true = "You can learn to program!"
+# active_false = "You won't have enough strength and patience, you won't be to")
+#
+# bool(active_true = True)
+# bool(active_false = False)
+#
+# ##6-задача
+# math_tasks = str("2 * 4 + 8 * 3")
+#
+#
+# ##7-задача
+# digits_1 = int(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+# digits_2 = float(1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9)
+#
+# ##8-задача
+# empty = str()
+
 ##1-задача
-my_name = input("Enter your name: ")
-age = int()
-my_weight = float(my_weight)
-weather = str("20")
 
-##2-задача
-my_review = str("I am exited to continue learning programming")
-
-##3-задача
-my_money_now = float(input())
-my_money_future = float(input())
-
-##4-задача
-my_age = int(input("Enter your age: ")
-my_birthday = int(input("Enter your birthday: ")
-
-
-##5-задача
-activ_true = "You can learn to program!"
-active_false = "You won't have enough strength and patience, you won't be to")
-
-bool(active_true = True)
-bool(active_false = False)
-
-##6-задача
-math_tasks = str("2 * 4 + 8 * 3")
-
-
-##7-задача
-digits_1 = int(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
-digits_2 = float(1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9)
-
-##8-задача
-empty = str()
+# car_model = "Subaru"
+# car_colour = "Black"
+# car_age = 2
+# car_doors = 4
+#
+# ##2-задача
+# length_rectangle = 6
+# width_rectangle = 8
+#
+# perimeter_rectangle = (length_rectangle + width_rectangle) * 2
+# print(perimeter_rectangle)
+#
+# area_rectangle = length_rectangle * width_rectangle
+# print(area_rectangle)
+#
+# ##3-задача
+# money_treasure = 2500
+# money_expense_everyday = 1500
+# money_already_in_the_chest = 100000
+#
+# daily_chance = (money_treasure - money_expense_everyday) * 365
+# all_money = daily_chance + 100000
+# print(all_money)

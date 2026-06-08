14ec664c648abea4e5859f63e6d9ca986457425f
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-06-08 21:25:05 +0500

    practice again

--- and/while in while and if in if.py
+++ and/while in while and if in if.py
@@ -62,41 +62,85 @@ while True:
 #Создайте программу, которая просит пользователя ввести числа, затем выводит на экран квадрат этого числа.
 #Если пользователь ввел не число, а что-то другое, программа должна вывести сообщение о некорректных данных, затем просить пользователя повторить ввод.
 
-1 VARIANT
-active = True
-
-while active:
-    number = input("Введите число: ")
-
-    if not number.isdigit():
-        print("Ошибка некорректных данных, повторите ввод: ")
-    else:
-        print(f"Квадрат этих чисел: {int(number) ** 2}")
-        active = False
-
-2.1 VARIANT
-number = input("Введите число: ") # "Hello"
-while not number.isdigit():
-    print("Ошибка - некорректные данные, повторите ввод")
-    number = input("Повторите ввод: ") # "12"
-
-print(f"Квадрат этого числа: {int(number) ** 2}")
-
-
+# 1 VARIANT
+# active = True
+#
+# while active:
+#     number = input("Введите число: ")
+#
+#     if not number.isdigit():
+#         print("Ошибка некорректных данных, повторите ввод: ")
+#     else:
+#         print(f"Квадрат этих чисел: {int(number) ** 2}")
+#         active = False
+#
 # 2.1 VARIANT
-number = input("Введите число: ")
+# number = input("Введите число: ") # "Hello"
+# while not number.isdigit():
+#     print("Ошибка - некорректные данные, повторите ввод")
+#     number = input("Повторите ввод: ") # "12"
+#
+# print(f"Квадрат этого числа: {int(number) ** 2}")
+#
+#
+# # 2.1 VARIANT
+# number = input("Введите число: ")
+#
+# while not number.isdigit():
+#     print("Ошибка некорректных данных, повторите ввод: ")
+#     number = input("Повторите ввод: ")
+# else:
+#     print(f"Квадрат этих чисел: {int(number) ** 2}")
+#
+# while True:
+#     number = input("Введите число: ")
+#
+#     if number.isdigit():    #мынау нени билдиреди
+#         break               #неушн нахуя колдандык
+#     print("Ошибка ввод данных, повторите ввод: ")
+#
+# print(f"Квадрат этих чисел: {int(number) ** 2}")
 
-while not number.isdigit():
-    print("Ошибка некорректных данных, повторите ввод: ")
-    number = input("Повторите ввод: ")
-else:
-    print(f"Квадрат этих чисел: {int(number) ** 2}")
+
+print("Здравствуйте, добро пожаловать в наш магазин.")
+
+print("\nВот список товаров: ")
+print("1. Сахар - 100 тг за кг")
+print("2. Конфеты - 300 тг за кг")
+print("3. Соль - 70 тг за кг")
+print("4. Хлеб - 80 тг за булку")
+print("5. Яйца - 220 тг за лоток")
+print()
 
 while True:
-    number = input("Введите число: ")
+    goods = input("Выберите товар: ")
+    quantity = input("Выберите количество: ")
 
-    if number.isdigit():    #мынау нени билдиреди
-        break               #неушн нахуя колдандык
-    print("Ошибка ввод данных, повторите ввод: ")
+    if goods == "end" and quantity == "end":
+        break
 
-print(f"Квадрат этих чисел: {int(number) ** 2}")
+    while not goods.isdigit() or not quantity.isdigit():
+        print("Ошибка - некорректные данные, повторите ввод!")
+        goods = input("Повторите ввод 1: ")
+        quantity = input("Повторите ввод 2: ")
+    else:
+        if goods == "1":
+            result1 = 100 * int(quantity)
+            print(result1)
+        if goods == "2":
+            result2 = 300 * int(quantity)
+            print(result2)
+        if goods == "3":
+            result3 = 70 * int(quantity)
+            print(result3)
+        if goods == "4":
+            result4 = 80 * int(quantity)
+            print(result4)
+        if goods == "5":
+            result5 = 220 * int(quantity)
+            print(result5)
+        if goods == "end" and quantity == "end":
+            break
+
+print()
+print("Общая стоимость: ")

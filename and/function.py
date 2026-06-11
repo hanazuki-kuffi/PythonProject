97d1ea48ac0de8c15d8e17aeee11f899d21881c6
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-06-11 22:59:17 +0500

    practice again

--- and/function.py
+++ and/function.py
@@ -11,3 +11,59 @@ def welcome_user(name, is_returning_user=True):
 welcome_user(name="Kuffi")
 welcome_user("c: c:", is_returning_user=True)
 welcome_user("Ali", False)
+
+
+
+one = True
+
+while one:
+    print("Регистрация\n")
+    name = input("Ваше имя: ")
+    password_ = input("Придумайте пароль: ")
+    password__ = input("Подтвердите пароль: ")
+
+    if password_ != password__:
+        print("Неверный пароль, начните заново!")
+    else:
+        print("Данные сохранены!\n")
+
+        while one:
+            print("Авторизация")
+
+            login = input("Введите логин: ")
+            password = input("Введите пароль: ")
+
+            if not (login == name and password == password_):
+                print("Вход не выполнен")
+            else:
+                print("Вход выполнен")
+                one = False
+
+
+
+def reg():
+    while True:
+        print("Регистрация\n")
+        login = input("Ваше имя: ")
+        password = input("Придумайте пароль: ")
+        password_ = input("Подтвердите пароль: ")
+
+        if password != password_:
+            print("Неверный пароль, начните заново!")
+        else:
+            print("Данные сохранены!\n")
+            return login + password
+
+
+def auto(user_data):
+    while True:
+        print("Авторизация")
+
+        login = input("Введите логин: ")
+        password = input("Введите пароль: ")
+
+        if not (login + password == user_data):
+            print("Вход не выполнен")
+        else:
+            print("Вход выполнен")
+            break

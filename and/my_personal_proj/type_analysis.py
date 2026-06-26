608713a41e16b090a1491a71c44f6989d3d210cf
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-06-26 14:06:03 +0500

    practice with list

---
+++ and/my_personal_proj/type_analysis.py
@@ -0,0 +1,8 @@
+while True:
+    print("\nType analysis:")
+    print()
+    print("1. NG, CT, MG, TV")
+    print("2. Candida.albicans")
+    print("3. D-Bioflor")
+    print("4. HPV-23")
+    print("5. HPV-18")

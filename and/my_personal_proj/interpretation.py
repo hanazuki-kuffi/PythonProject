4d750a2cc460bdd18c91cd6be64afdd43ea213f8
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-06-28 20:14:37 +0500

    practice with list and write data patient results

--- and/my_personal_proj/interpretation.py
+++ and/my_personal_proj/interpretation.py
@@ -1,5 +1,18 @@
 barcode_patient = ["415893", "209970", "508988", "804719", "419974", "805466", "212383", "514252"]
 
 folder_results = [
-    []
+    [None, None, None, 28.73, None]
+    [None, 29.31, None, 26.51, None]
+    [None, 29.70, None, 38.76, None]
+    [19.59, None, None, 33.5, None]
+    [24.23, None, None, 32.78, None]
+    [None, None, 31.01, 28.77, None]
+    [None, None, None, 27.72, None]
+    [None, None, None, 40.01, None]
+]
+
+matrix = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]
 ]

73cec9e5bef64418896b7d184d3d6869efb25b2f
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-06-29 12:08:39 +0500

    practice with list and write data patient results

--- and/my_personal_proj/interpretation.py
+++ and/my_personal_proj/interpretation.py
@@ -1,18 +1,12 @@
 barcode_patient = ["415893", "209970", "508988", "804719", "419974", "805466", "212383", "514252"]
 
 folder_results = [
-    [None, None, None, 28.73, None]
-    [None, 29.31, None, 26.51, None]
-    [None, 29.70, None, 38.76, None]
-    [19.59, None, None, 33.5, None]
-    [24.23, None, None, 32.78, None]
-    [None, None, 31.01, 28.77, None]
-    [None, None, None, 27.72, None]
-    [None, None, None, 40.01, None]
-]
-
-matrix = [
-[1, 2, 3],
-[4, 5, 6],
-[7, 8, 9]
+    [None, None, None, 28.73, None], # 415893               ставить запятую после списка очень важно ведь список работает таким образом чтоб не вывести синтакситеческих ошибок
+    [None, 29.31, None, 26.51, None], # 209970
+    [None, 29.70, None, 38.76, None], # 508988
+    [19.59, None, None, 33.5, None],  # 804719
+    [24.23, None, None, 32.78, None], # 419974
+    [None, None, 31.01, 28.77, None], # 805466
+    [None, None, None, 27.72, None],  # 212383
+    [None, None, None, 40.01, None]   # 514252
 ]

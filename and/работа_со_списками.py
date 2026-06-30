51306f65dc49d29b140c91d5ef76586b87333f48
hanazuki-kuffi <tuakbaevagulajym@gmail.com>
2026-06-30 20:24:59 +0500

    practice with MY PCR PROJECTS

--- and/работа_со_списками.py
+++ and/работа_со_списками.py
@@ -1,34 +1,34 @@
 # 1. Создай список из 5 любимых фруктов и выведи каждый элемент через цикл for
 
-my_favorite_fruits = ["banana", "apple", "peach", "persimmon", "berries"]
-
-for fruit in my_favorite_fruits:
-    print(fruit.title())     #"hello world".capitalize() ➞ "Hello world" = делает заглавной только первую букву самой строки
+# my_favorite_fruits = ["banana", "apple", "peach", "persimmon", "berries"]
+#
+# for fruit in my_favorite_fruits:
+#     print(fruit.title())     #"hello world".capitalize() ➞ "Hello world" = делает заглавной только первую букву самой строки
                              #"hello world".title() ➞ "Hello World" =  делает заглавной первую букву каждого слова
 
 # 2. Создай список из 5 чисел и выведи первый и последний элемент
 
-numbers = [1, 2, 3, 4, 5]
-
-del numbers[1:4]
-print(numbers)
+# numbers = [1, 2, 3, 4, 5]
+#
+# del numbers[1:4]
+# print(numbers)
 
 # 3. Создай список из 4 имён и добавь ещё одно имя в конец через append()
 
-names = ["ali", "nurailym", "kawai", "arai"]
-
-names.append("kuffi")
-
-print(names)
+# names = ["ali", "nurailym", "kawai", "arai"]
+#
+# names.append("kuffi")
+#
+# print(names)
 
 
 # 4. Создай список из 6 чисел и выведи только первые три через срез
 
-numbers = [1, 2, 3, 4, 5, 6]
-
-new_list_numbers = numbers[0:3]
-
-print(new_list_numbers)
+# numbers = [1, 2, 3, 4, 5, 6]
+#
+# new_list_numbers = numbers[0:3]
+#
+# print(new_list_numbers)
 
 
 
@@ -36,8 +36,6 @@ print(new_list_numbers)
 
 numbers = [1, 2, 3, 4, 5]
 
-
-
 for number in numbers:
-    print(number, end=" + ")
-print(number)
+    print(number)
+
